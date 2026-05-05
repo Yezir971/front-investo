719f33d9a65c36a662e3c3b4daeddbaae71539f5
Yezir971 <james_ahmedaly@yahoo.com>
2026-05-05 12:22:59 +0200

    feat : add login

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,20 +1,42 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { AuthService } from '../../services/auth';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { email, form, FormField, required } from '@angular/forms/signals';
+
+
+interface LoginData {
+  email: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormsModule, FormField],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  loginModel = signal<LoginData>({
+    email:'',
+    password:'',
+  })
+
+  loginForm = form(this.loginModel, (schemaPath)=> {
+    required(schemaPath.email, {message : 'Email est obligatoire.'})
+    email(schemaPath.email, {message: "Email n'est pas valide." })
+    required(schemaPath.password, {message : 'Mot de passe est obligatoire'})
+  })
+
+
   private router = inject(Router);
   private authService = inject(AuthService)
-  credentials = {email: "", password: ""}
-  onSubmit(){
-    this.authService.login(this.credentials).subscribe({
+
+
+  onSubmit(event : Event){
+    event.preventDefault();
+    const credentials = this.loginModel()
+    this.authService.login(credentials).subscribe({
       next: () => {
         this.router.navigate(['']);
       }
